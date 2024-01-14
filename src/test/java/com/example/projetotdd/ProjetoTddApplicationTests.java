f1598fd8e9950fa84fc9985e0c7af8dd01c2f1ef
Lucas Araujo <assuncao.araujo.lucas@gmail.com>
2024-01-14 12:00:30 -0300

    commit files API TDD

---
+++ src/test/java/com/example/projetotdd/ProjetoTddApplicationTests.java
@@ -0,0 +1,24 @@
+package com.example.projetotdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+
+}
+
+/**api
+ * 
+ * Criar uma api de vendas
+ * Venda igual ou acima de 1000,00 = 15%
+ * vendas abaixo de 1000,00 = 10%
+ * 
+ * Classe Comissao que tenha um metodo calcular() que 
+ * saiba devolver o valor da comissao 
+ */
---
+++ src/test/java/com/example/projetotdd/controller/ProdutoControllerTest.java
@@ -0,0 +1,108 @@
+package com.example.projetotdd.controller;
+
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.example.projetotdd.model.Produto;
+import com.example.projetotdd.service.ProdutoService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@WebMvcTest
+public class ProdutoControllerTest {
+    // MockMvc
+    // Mockito
+    // Mock
+
+    @Autowired
+    private ProdutoController produtoController;
+
+    @MockBean
+    private ProdutoService produtoService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        // Aqui será executado antes de qualquer caso de teste
+        this.mockMvc = MockMvcBuilders.standaloneSetup(produtoController).build();
+    }
+
+    @Test
+    public void deve_retornar_status_OK_ao_chamar_metodo_obter_todos() throws Exception {
+        // Arrange = Preparação
+        List<Produto> produtos = new ArrayList<Produto>();
+        var requestBuilder = MockMvcRequestBuilders.get("/api/produtos");
+        when(this.produtoService.obterTodos()).thenReturn(produtos);
+
+        // Act = Ação
+        this.mockMvc.perform(requestBuilder)
+           
+        // Assert = Confirmação
+         .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+
+    @Test
+    public void deve_retornar_o_produto_por_id() throws Exception {
+        // Arrange = Preparação
+        Produto produto = new Produto();
+        produto.setId(2l);
+        produto.setNome("Martelo");
+        produto.setQuantidade(10);
+
+        Optional<Produto> optProduto = Optional.of(produto);
+
+        var requestBuilder = MockMvcRequestBuilders.get("/api/produtos/2");
+
+        when(this.produtoService.obterPorId(2l)).thenReturn(optProduto);
+    
+        // Act = Ação
+        this.mockMvc.perform(requestBuilder)
+           
+        // Assert = Confirmação
+         .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(2l));
+    }
+
+      @Test
+    public void deve_adicionar_o_produto() throws Exception {
+        // Arrange = Preparação
+        //Criando o produto que será adicionado
+        Produto produto = new Produto();
+        produto.setNome("Martelo");
+        produto.setQuantidade(10);
+
+        // Convertendo o produto em JSON
+        String json = new ObjectMapper().writeValueAsString(produto);
+
+        //criando a requisição do tipo POST 
+        var requestBuilder = MockMvcRequestBuilders.post("/api/produtos")
+        .content(json)
+        .contentType(MediaType.APPLICATION_JSON);
+
+        //Adicionando o ID ao produto que vamos retornar 
+        produto.setId(1l);
+
+        when(this.produtoService.adicionar(produto)).thenReturn(produto);
+    
+        // Act = Ação
+        this.mockMvc.perform(requestBuilder)
+           
+        // Assert = Confirmação
+         .andExpect(MockMvcResultMatchers.status().isCreated());
+    }
+}
